744e0885158991f71a7aa20a77d986f895b27522
Rishav Sanjay <83537945+rishavsanjay@users.noreply.github.com>
2026-08-06 15:54:05 +0530

    radeon forge: export safe deployment policy

--- extra/radeon_forge/synthesis/__init__.py
+++ extra/radeon_forge/synthesis/__init__.py
@@ -1,5 +1,6 @@
 from .autotune import SearchPlan, run_autotune
 from .defaults import install_default_specs
+from .deployment import RUNTIME_ADAPTERS, STATEFUL_LAYERS, SafeHookRegistry
 from .hooks import (ActiveHook, CompatibilityReport, ExecutionContext, ExecutionStage, HookDescriptor, HookLayer, HookMode,
                     HookRegistry, RuntimeFingerprint, StagePredicate, check_compatibility)
 from .portable import export_recipe_with_hook
@@ -9,5 +10,6 @@ from .workspace import CandidateRecord, CandidateWorkspace, KernelSpec
 
 __all__ = ["ActiveHook", "CandidateRecord", "CandidateWorkspace", "CompatibilityReport", "ExecutionContext", "ExecutionStage",
            "ForgeRecipe", "HookDescriptor", "HookLayer", "HookMode", "HookRegistry", "InstalledRecipe", "KernelSpec",
-           "OptimizationTools", "RecipeArtifact", "RecipeLibrary", "RuntimeFingerprint", "SearchPlan", "StagePredicate",
-           "check_compatibility", "export_recipe", "export_recipe_with_hook", "install_default_specs", "run_autotune"]
+           "OptimizationTools", "RUNTIME_ADAPTERS", "RecipeArtifact", "RecipeLibrary", "RuntimeFingerprint", "STATEFUL_LAYERS",
+           "SafeHookRegistry", "SearchPlan", "StagePredicate", "check_compatibility", "export_recipe", "export_recipe_with_hook",
+           "install_default_specs", "run_autotune"]
