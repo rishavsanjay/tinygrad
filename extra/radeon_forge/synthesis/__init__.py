76b872fb4262de32c0e0238c4df15378f9bff59d
Rishav Sanjay <83537945+rishavsanjay@users.noreply.github.com>
2026-08-06 15:23:05 +0530

    radeon forge: export execution-stage hook contracts

--- extra/radeon_forge/synthesis/__init__.py
+++ extra/radeon_forge/synthesis/__init__.py
@@ -1,9 +1,12 @@
 from .defaults import install_default_specs
-from .hooks import ActiveHook, CompatibilityReport, HookDescriptor, HookLayer, HookMode, HookRegistry, RuntimeFingerprint, check_compatibility
+from .hooks import (ActiveHook, CompatibilityReport, ExecutionContext, ExecutionStage, HookDescriptor, HookLayer, HookMode,
+                    HookRegistry, RuntimeFingerprint, StagePredicate, check_compatibility)
+from .portable import export_recipe_with_hook
 from .recipe import ForgeRecipe, InstalledRecipe, RecipeArtifact, RecipeLibrary, export_recipe
 from .tools import OptimizationTools
 from .workspace import CandidateRecord, CandidateWorkspace, KernelSpec
 
-__all__ = ["ActiveHook", "CandidateRecord", "CandidateWorkspace", "CompatibilityReport", "ForgeRecipe", "HookDescriptor",
-           "HookLayer", "HookMode", "HookRegistry", "InstalledRecipe", "KernelSpec", "OptimizationTools", "RecipeArtifact",
-           "RecipeLibrary", "RuntimeFingerprint", "check_compatibility", "export_recipe", "install_default_specs"]
+__all__ = ["ActiveHook", "CandidateRecord", "CandidateWorkspace", "CompatibilityReport", "ExecutionContext", "ExecutionStage",
+           "ForgeRecipe", "HookDescriptor", "HookLayer", "HookMode", "HookRegistry", "InstalledRecipe", "KernelSpec",
+           "OptimizationTools", "RecipeArtifact", "RecipeLibrary", "RuntimeFingerprint", "StagePredicate", "check_compatibility",
+           "export_recipe", "export_recipe_with_hook", "install_default_specs"]
