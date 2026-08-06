dd3fcaa41cebb8c74d3ddffb11388344e7cb779d
Rishav Sanjay <83537945+rishavsanjay@users.noreply.github.com>
2026-08-06 15:19:32 +0530

    radeon forge: export runtime hook contracts

--- extra/radeon_forge/synthesis/__init__.py
+++ extra/radeon_forge/synthesis/__init__.py
@@ -1,7 +1,9 @@
 from .defaults import install_default_specs
+from .hooks import ActiveHook, CompatibilityReport, HookDescriptor, HookLayer, HookMode, HookRegistry, RuntimeFingerprint, check_compatibility
 from .recipe import ForgeRecipe, InstalledRecipe, RecipeArtifact, RecipeLibrary, export_recipe
 from .tools import OptimizationTools
 from .workspace import CandidateRecord, CandidateWorkspace, KernelSpec
 
-__all__ = ["CandidateRecord", "CandidateWorkspace", "ForgeRecipe", "InstalledRecipe", "KernelSpec", "OptimizationTools",
-           "RecipeArtifact", "RecipeLibrary", "export_recipe", "install_default_specs"]
+__all__ = ["ActiveHook", "CandidateRecord", "CandidateWorkspace", "CompatibilityReport", "ForgeRecipe", "HookDescriptor",
+           "HookLayer", "HookMode", "HookRegistry", "InstalledRecipe", "KernelSpec", "OptimizationTools", "RecipeArtifact",
+           "RecipeLibrary", "RuntimeFingerprint", "check_compatibility", "export_recipe", "install_default_specs"]
